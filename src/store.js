7155fdf5329bb996f123812e0e54a9a2d977a942
Beck TJ (Bakytbek Tatibekov) <22246818+beckinfonet@users.noreply.github.com>
2018-08-04 23:21:11 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -7,18 +7,19 @@ export default store; //The store is used in index.js by the react-redux top lev
 
 
 //STORE WITH MIDDLEWARE
-// import { createStore } from 'redux'; //Notice redux, not react-redux
-// import reducer from './reducer';
-// import promiseMiddleware from 'redux-promise-middleware';
+```
+import { createStore } from 'redux'; //Notice redux, not react-redux
+import reducer from './reducer';
+import promiseMiddleware from 'redux-promise-middleware';
 
-// let createStoreWithMiddleware = applyMiddleware(
-//   promiseMiddleware()
-// )(createStore);
+let createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware()
+)(createStore);
 
-// let store = createStoreWithMiddleware(reducer);
+let store = createStoreWithMiddleware(reducer);
 
-// export default store; //The store is used in index.js by the react-redux top level provider
-
+export default store; //The store is used in index.js by the react-redux top level provider
+```
 
 
 
